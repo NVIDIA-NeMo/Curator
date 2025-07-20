66b7d45708741ee1265c9f76c1d661d601722144
Sarah Yurick <sarahyurick@gmail.com>
2025-07-19 18:15:08 -0700

    first pass for entire arxiv implementation
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>

--- ray-curator/ray_curator/stages/download/text/__init__.py
+++ ray-curator/ray_curator/stages/download/text/__init__.py
@@ -1,7 +1,9 @@
 from .base import DocumentDownloader, DocumentDownloadExtractStage, DocumentExtractor, DocumentIterator, URLGenerator
 from .common_crawl.stage import CommonCrawlDownloadExtractStage
+from .arxiv.stage import ArxivDownloadExtractStage
 
 __all__ = [
+    "ArxivDownloadExtractStage",
     "CommonCrawlDownloadExtractStage",
     "DocumentDownloadExtractStage",
     "DocumentDownloader",
