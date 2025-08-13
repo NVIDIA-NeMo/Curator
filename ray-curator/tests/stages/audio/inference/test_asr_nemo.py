410cd88efc1909dd664b331169ded817b2647269
Nikolay Karpov <nkarpov@nvidia.com>
2025-08-12 17:12:48 -0700

    test_process_success
    Signed-off-by: Nikolay Karpov <nkarpov@nvidia.com>

--- ray-curator/tests/stages/audio/inference/test_asr_nemo.py
+++ ray-curator/tests/stages/audio/inference/test_asr_nemo.py
@@ -1,8 +1,12 @@
 """Test suite for InferenceAsrNemoStage."""
 
 import os
+from unittest.mock import patch
+
+import pytest
 
 from ray_curator.stages.audio.inference.asr_nemo import InferenceAsrNemoStage
+from ray_curator.tasks import SpeechObject
 
 
 def get_e2e_test_data_path() -> str:
@@ -22,9 +26,9 @@ class TestAsrNeMoStage:
     def test_stage_properties(self) -> None:
         """Test stage properties."""
         stage = InferenceAsrNemoStage(model_name="nvidia/parakeet-tdt-0.6b-v2")
-        assert stage.name == "audio_inference"
-        assert stage.inputs() == ([], [])
-        assert stage.outputs() == (["data"], ["audio_filepath", "text"])
+        assert stage.name == "ASR_inference"
+        assert stage.inputs() == (["data"], [])
+        assert stage.outputs() == (["data"], ["audio_filepath", "pred_text"])
 
     def test_stage_initialization(self) -> None:
         """Test stage initialization with different parameters."""
@@ -38,3 +42,34 @@ class TestAsrNeMoStage:
             model_name="nvidia/parakeet-tdt-0.6b-v2",
         )
         assert stage.batch_size == 16
+
+    @patch.object(InferenceAsrNemoStage, "transcribe", return_value=["the cat", "set on a mat"])
+    @pytest.mark.usefixtures("_")
+    def test_process_success(self) -> None:
+        """Test process method with successful file discovery."""
+
+        stage = InferenceAsrNemoStage(model_name="nvidia/parakeet-tdt-0.6b-v2")
+
+        path_list = [
+            SpeechObject(data={"audio_filepath": "/test/audio1.wav"}),
+            SpeechObject(data={"audio_filepath": "/test/audio2.mp3"}),
+        ]
+
+        stage.setup_on_node()
+        stage.setup()
+        result = stage.process_batch(path_list)
+
+        assert len(result) == 2
+        assert all(isinstance(task, SpeechObject) for task in result)
+        assert result[0].task_id == "task_id_/test/audio1.wav"
+        assert result[1].task_id == "task_id_/test/audio2.mp3"
+        assert result[0].dataset_name == "nvidia/parakeet-tdt-0.6b-v2_inference"
+        assert result[1].dataset_name == "nvidia/parakeet-tdt-0.6b-v2_inference"
+
+        # Check that the audio objects are created correctly
+        assert isinstance(result[0].data, dict)
+        assert isinstance(result[1].data, dict)
+        assert result[0].data[result[0].filepath_key] == "/test/audio1.wav"
+        assert result[0].data["pred_text"] == "the cat"
+        assert result[1].data[result[1].filepath_key] == "/test/audio2.mp3"
+        assert result[1].data["pred_text"] == "set on a mat"
