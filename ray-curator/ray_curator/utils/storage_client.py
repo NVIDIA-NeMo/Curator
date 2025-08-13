33ed63316f69b026dee014c26e63d1e36aa6a27d
Ao Tang <aot@nvidia.com>
2025-08-13 09:25:55 -0700

    Remove SPDX license comments from S3 client, storage client, and storage utilities files to streamline code readability. This change simplifies the file headers while retaining essential module documentation.
    Signed-off-by: Ao Tang <aot@nvidia.com>

--- ray-curator/ray_curator/utils/storage_client.py
+++ ray-curator/ray_curator/utils/storage_client.py
@@ -1,24 +1,3 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""Base storage client interface for various storage systems.
-
-This module provides a base interface for storage client implementations
-(S3, Azure Blob Storage, etc.) with common operations for interacting
-with cloud storage systems.
-"""
-
 import abc
 import concurrent.futures
 import pathlib
