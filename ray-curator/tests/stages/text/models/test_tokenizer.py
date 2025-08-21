e364feb3e5974365b9584f2b332ed5194f4564e8
Sarah Yurick <sarahyurick@gmail.com>
2025-08-21 15:59:12 -0700

    sort
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>

--- ray-curator/tests/stages/text/models/test_tokenizer.py
+++ ray-curator/tests/stages/text/models/test_tokenizer.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
 import importlib
+import sys
 from typing import Any
 from unittest.mock import Mock, patch
 
