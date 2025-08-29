288b455672c3b5b82d7348b82f129ecffb8ce19a
Sarah Yurick <sarahyurick@gmail.com>
2025-08-29 10:38:36 -0700

    edit task init
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>

--- nemo_curator/tasks/__init__.py
+++ nemo_curator/tasks/__init__.py
@@ -12,12 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .audio import AudioBatch
 from .document import DocumentBatch
 from .file_group import FileGroupTask
 from .image import ImageBatch, ImageObject
 from .tasks import EmptyTask, Task, _EmptyTask
 
 __all__ = [
+    "AudioBatch",
     "DocumentBatch",
     "EmptyTask",
     "FileGroupTask",
