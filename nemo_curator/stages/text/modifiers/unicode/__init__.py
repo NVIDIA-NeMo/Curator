d92d4dd7d27d165deff16588ffc429e6d6c6ef60
Kunal Sachdev <kunalmgsachdev@gmail.com>
2026-02-09 23:24:35 -0500

    Renamed nemo_curator/stages/text/modifiers/unicode/reformatter.py to unicode_reformatter.py becuase all modifiers are text reformatters (#1232)
    Signed-off-by: Kunal Sachdev <kunalmgsachdev@gmail.com>

--- nemo_curator/stages/text/modifiers/unicode/__init__.py
+++ nemo_curator/stages/text/modifiers/unicode/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .reformatter import UnicodeReformatter
+from .unicode_reformatter import UnicodeReformatter
 
 __all__ = [
     "UnicodeReformatter",
