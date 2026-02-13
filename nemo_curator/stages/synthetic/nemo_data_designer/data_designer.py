9f62ba175aade6694224645f720df10921d291e3
Huy Vu2 <huvu@login-eos02.eos.clusters.nvidia.com>
2026-02-12 21:54:23 -0800

    lin

--- nemo_curator/stages/synthetic/nemo_data_designer/data_designer.py
+++ nemo_curator/stages/synthetic/nemo_data_designer/data_designer.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from dataclasses import dataclass, field
 import logging
 import time
-from dataclasses import dataclass, field
 
 import data_designer.config as dd
 from data_designer.interface import DataDesigner
