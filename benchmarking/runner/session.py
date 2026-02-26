1b4ab07e4d38c9c9254b6198ad12a180dafbcf8c
rlratzel <rratzel@nvidia.com>
2026-02-26 15:11:40 -0600

    Adds 'name' as a required key in a dict used to create a Session obj.
    Signed-off-by: rlratzel <rratzel@nvidia.com>

--- benchmarking/runner/session.py
+++ benchmarking/runner/session.py
@@ -86,7 +86,7 @@ class Session:
     @classmethod
     def assert_valid_config_dict(cls, data: dict) -> None:
         """Assert that the configuration contains the minimum required config values."""
-        required_fields = ["results_path", "datasets_path", "model_weights_path", "entries"]
+        required_fields = ["name", "results_path", "datasets_path", "model_weights_path", "entries"]
         missing_fields = [k for k in required_fields if k not in data]
         if missing_fields:
             msg = f"Invalid configuration: missing required fields: {missing_fields}"
