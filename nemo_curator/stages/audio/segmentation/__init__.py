7f6b8cd35bf12540c819a71c6d43f4abfdb9ae20
shbhawsar <shbhawsar@nvidia.com>
2026-03-05 02:26:08 -0800

    Add SpeakerSeparationStage

---
+++ nemo_curator/stages/audio/segmentation/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, NVIDIA CORPORATION.  All rights reserved.
+# Licensed under the Apache License, Version 2.0
+
+"""Audio segmentation stages."""
+
+from .speaker_separation import SpeakerSeparationStage
+
+__all__ = ["SpeakerSeparationStage"]
