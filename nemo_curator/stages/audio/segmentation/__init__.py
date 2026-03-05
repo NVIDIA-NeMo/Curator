bf035d986356fc9fd92b2755a01fef3b2e89e916
shbhawsar <shbhawsar@nvidia.com>
2026-03-05 02:25:21 -0800

    Add VADSegmentationStage

---
+++ nemo_curator/stages/audio/segmentation/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, NVIDIA CORPORATION.  All rights reserved.
+# Licensed under the Apache License, Version 2.0
+
+"""Audio segmentation stages."""
+
+from .vad_segmentation import VADSegmentationStage
+
+__all__ = ["VADSegmentationStage"]
