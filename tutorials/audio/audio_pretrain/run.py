02b9bdeeedd64c88720b88fc728955724b468199
Yongqiang Wang <yongqiang.seagull@gmail.com>
2026-05-04 13:56:09 -0700

    update tutorials/audio/audio_pretrain/run.py
    Signed-off-by: Yongqiang Wang <yongqiang.seagull@gmail.com>

--- tutorials/audio/audio_pretrain/run.py
+++ tutorials/audio/audio_pretrain/run.py
@@ -51,9 +51,9 @@ from nemo_curator.stages.audio.alm.pretrain import (
 )
 
 _EXECUTOR_FACTORIES = {
-    "xenna": "nemo_curator.backends.xenna:XennaExecutor",
-    "ray_data": "nemo_curator.backends.experimental.ray_data:RayDataExecutor",
-    "ray_actor_pool": "nemo_curator.backends.experimental.ray_actor_pool:RayActorPoolExecutor",
+    "xenna": "nemo_curator.backends.xenna.executor:XennaExecutor",
+    "ray_data": "nemo_curator.backends.ray_data.executor:RayDataExecutor",
+    "ray_actor_pool": "nemo_curator.backends.ray_actor_pool.executor:RayActorPoolExecutor",
 }
 
 
