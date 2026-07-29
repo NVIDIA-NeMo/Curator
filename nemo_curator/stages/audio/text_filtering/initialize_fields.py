94a4e7922dedca73f90e73026a3c0b7c95a7e50c
aaftaabv@gmail.com <aaftaabv@gmail.com>
2026-07-29 15:48:47 +0530

    Reuse default batch processing for field initialization

--- nemo_curator/stages/audio/text_filtering/initialize_fields.py
+++ nemo_curator/stages/audio/text_filtering/initialize_fields.py
@@ -97,8 +97,3 @@ class InitializeFieldsStage(ProcessingStage[AudioTask, AudioTask]):
     def process(self, task: AudioTask) -> AudioTask:
         self._initialize(task)
         return task
-
-    def process_batch(self, tasks: list[AudioTask]) -> list[AudioTask]:
-        for task in tasks:
-            self._initialize(task)
-        return tasks
